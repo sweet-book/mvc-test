c064aabfa4808e60e5a0aa01e326735c06cc4378
amanagi-sigure <mafunyang@gmail.com>
2019-12-19 13:30:43 +0900

    1

---
+++ mvc/Controllers/srcController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc.Controllers
+{
+    public class srcController : Controller
+    {
+        public ActionResult Index(DateTime? start, DateTime? end)
+        {
+            if (!start.HasValue && !end.HasValue)
+            {
+                ViewBag.StartDate = DateTime.Parse("2019-06-01");
+                ViewBag.EndDate = DateTime.Parse(DateTime.Now.ToString("yyyy-MM-dd") + " 23:59:59.000");
+            }
+            else
+            {
+                ViewBag.StartDate = start;
+                ViewBag.EndDate = end;
+            }
+            return View();
+        }
+    }
+}
---
+++ mvc/Models/regist.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace mvc.Models
+{
+    public class regist
+    {
+        public int num { get; set; }
+        public string combine_code { get; set; }
+        public string company_code { get; set; }
+        public string booktitle { get; set; }
+        public int productcode { get; set; }
+        public int goodid { get; set; }
+        public int ea { get; set; }
+        public string filename { get; set; }
+        public DateTime date { get; set; }
+        public string result { get; set; }
+        public string userid { get; set; }
+        public string name { get; set; }
+        public double money { get; set; }
+        public double besongmoney { get; set; }
+    }
+
+    public class registDBContext : DbContext
+    {
+        public DbSet<regist> Movies { get; set; }
+    }
+}
